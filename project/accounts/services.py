08925e7dafbc6ff52825426fcf6f15ead94dc2dd
udkumar <udkumar@hotmail.com>
2021-08-18 19:28:24 +0530

    currentcy changed into smallest unit

--- project/accounts/services.py
+++ project/accounts/services.py
@@ -2,6 +2,7 @@ from .models import Account, Transaction
 
 
 def perform_deposit(user, amount):
+    amount = btc_to_satoshis(amount)
     account = Account.objects.get(user=user)
     account.balance += amount
     account.save()
@@ -16,3 +17,7 @@ def perform_deposit(user, amount):
 
 def perform_withdrawal(user, amount):
     pass
+
+def btc_to_satoshis(amount):
+    amount = amount*100000000
+    return amount
