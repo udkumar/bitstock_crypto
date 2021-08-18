02bc0bf8b411877e83e42ef84862be33d0371484
udkumar <udkumar@hotmail.com>
2021-08-18 19:44:12 +0530

    added exception handling

--- project/accounts/services.py
+++ project/accounts/services.py
@@ -22,20 +22,23 @@ def perform_deposit(user, amount):
         print("Something went wrong with amount deposit")
 
 def perform_withdrawal(user, amount):
-    amount = btc_to_satoshis(amount)
-    account = Account.objects.get(user=user)
-    if account:
-        transaction = Transaction.objects.create(
-            account=account,
-            amount=amount,
-            transaction_type=Transaction.TRANSACTION_TYPE_WITHDRAWAL
-        )
-        if transaction:
-            account.balance -= amount
-            account.updated_at = datetime.datetime.now
-            account.save()
+    try:
+        amount = btc_to_satoshis(amount)
+        account = Account.objects.get(user=user)
+        if account:
+            transaction = Transaction.objects.create(
+                account=account,
+                amount=amount,
+                transaction_type=Transaction.TRANSACTION_TYPE_WITHDRAWAL
+            )
+            if transaction:
+                account.balance -= amount
+                account.updated_at = datetime.datetime.now
+                account.save()
 
-        return transaction
+            return transaction
+    except:
+        print("Something went wrong with withdrawal")
 
 def btc_to_satoshis(btc):
     satoshis = btc*100000000
